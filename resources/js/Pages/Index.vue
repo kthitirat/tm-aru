e50132857500a7a696dd955664563596ffe1f098
kthitirat <kthitirat@aru.ac.th>
2024-05-10 14:50:08 +0700

    feat: add effect

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -6,20 +6,18 @@
                 <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-blue-600">(PowerPoint)</h1>
             </div>
             <div class="grid grid-cols-4 gap-8 px-20 mt-10">
-                <div class="w-full  border border-slate-200 p-4" >
+                <div class="w-full  border border-slate-200 p-4 font-bold text-gray-700 hover:text-blue-600 hover:shadow-lg transition-all ease-in-out duration-500 cursor-pointer" >
                     <img class="w-full h-[400px] object-cover" src="https://picsum.photos/400/800">
                     <div class="mt-2">
-                        <p class="text-lg font-bold text-gray-700 text-blue-600 text-center leading-5">รายวิชา GE64303 การออมและการลงทุน</p>
-                        <div class="text-sm text-center mt-4">
+                        <p class="text-lg text-center leading-5">รายวิชา GE64303 การออมและการลงทุน</p>
+                        <div class="text-sm text-center mt-4 text-gray-700 font-normal">
                             <p>อาจารย์ ดร.มรุต กลัดเจริญ</p>
                             <p>คณะวิทยาการจัดการ</p>
                             <p>เผยแพร่วันที่ 1 มีนาคม 2567</p>
                         </div>
-
                     </div>
-                    <p></p>
-
                 </div>
+
 
             </div>
         </div>
