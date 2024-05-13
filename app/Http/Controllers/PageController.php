006e2b091bd41bdae949594e0349e5def3b0e7e4
kthitirat <kthitirat@aru.ac.th>
2024-05-13 11:41:45 +0700

    feat: add pagination index

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -16,7 +16,8 @@ class PageController extends Controller
     {
         $subjects = Subject::with('professors')
             ->whereNotNull('published_at')
-            ->paginate(50);
+            ->orderBy('published_at', 'desc')
+            ->paginate(5);
         $subjectsData = fractal($subjects,new SubjectTransformer())->toArray();
         return Inertia::render('Index')->with([
             'subjects' => $subjectsData
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -5,11 +5,24 @@
                 <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-700">เอกสารประกอบการสอน</h1>
                 <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-blue-600">(PowerPoint)</h1>
             </div>
-            <div class="grid grid-cols-4 gap-8 px-20 mt-10">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4 md:px-6 lg:px-10 xl:px-16 mt-10">
                 <div v-for="(subject,index) in subjectData" :key="subject.id">
                     <TeachingMaterialCard :subject="subject" />
                 </div>
             </div>
+            <div v-if="pagination !==null" class="px-4 md:px-6 lg:px-10 xl:px-16 mt-4 flex justify-end">
+                <!-- {{ pagination.links }} -->
+                <div class="join">
+                    <Link v-for="(pagination,index) in pagination.links" :key="index"
+                        :class="pagination.active ? 'btn-active':''"
+                        :href="pagination.url"
+                        class="join-item btn">
+                        {{pagination.label}}
+                    </Link>
+
+                </div>
+            </div>
+
         </div>
     </Layout>
 </template>
@@ -23,7 +36,7 @@ import {router} from "@inertiajs/vue3";
 
 export default {
     name: "Index",
-    components: {Layout,TeachingMaterialCard},
+    components: {Layout,TeachingMaterialCard,Link},
     props: {
         subjects:{
             type:Object,
@@ -34,13 +47,13 @@ export default {
     data() {
         return{
             subjectData: null,
-            subjectPagination: null,
+            pagination: null,
         };
 
     },
     mounted() {
         this.subjectData = this.subjects.data
-        this.subjectPagination = this.subjects.meta.pagination;
+        this.pagination = this.subjects.meta.pagination;
 
         // console.log('---------------');
         // console.log(this.subjectPagination);
